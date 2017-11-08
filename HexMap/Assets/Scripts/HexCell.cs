bb7cd74e107ab57149ab9d80b6c3051ec752edab
Erric <mateixzxf@icloud.com>
2017-11-08 10:21:45 +0800

    提交hexmap

---
+++ HexMap/Assets/Scripts/HexCell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+
+	public HexCoordinates coordinates;
+
+	public Color color;
+
+
+    [SerializeField]
+    HexCell[] Neighbours;
+
+
+    public HexCell GetHexNeighbour(HexDir dir)
+    {
+        return Neighbours[(int)dir];
+    }
+
+    public void SetHexNeighbour(HexDir dir, HexCell cell)
+    {
+        Neighbours[(int)dir] = cell;
+        cell.Neighbours[(int)dir.Opposite()] = this;
+    }
+
+}
---
+++ HexMap/Assets/Scripts/HexDirection.cs
@@ -0,0 +1,15 @@
+﻿
+public enum HexDir
+{
+    NE, E, SE, SW, W, NW,
+}
+
+//static extension method.
+public static class HexDirectionExtensions
+{
+    public static HexDir Opposite(this HexDir dir)
+    {
+        return (int)dir < 3 ? (dir + 3) : (dir - 3);
+    }
+}
+
