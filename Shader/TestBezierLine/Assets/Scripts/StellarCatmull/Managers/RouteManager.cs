805546995a02efd86aed1296b95c3e060d0467d9
Erric <mateixzxf@icloud.com>
2017-12-05 16:03:43 +0800

    提交Stellar

--- Shader/TestBezierLine/Assets/Scripts/StellarCatmull/Managers/RouteManager.cs
+++ Shader/TestBezierLine/Assets/Scripts/StellarCatmull/Managers/RouteManager.cs
@@ -9,11 +9,10 @@ namespace Assets.Scripts.Stellar
 
         public Transform m_tMajor;
 
-        public Transform m_tMajorPathPoints;
-
         public float m_fSpeed = 2f;
 
-        Vector3[] PathPoints;
+        public Stellar m_cStellar;
+
         float m_fCurPer = 0f;
         // Use this for initialization
         void Start()
@@ -32,21 +31,20 @@ namespace Assets.Scripts.Stellar
                 m_fCurPer = 0f;
 
             //读取曲线位置.
-            m_tMajor.transform.position = Stellar.StellarInst.Interp(PathPoints, m_fCurPer);
+            m_tMajor.transform.position = m_cStellar.Interp(m_fCurPer);
             //读取曲线方向.
-            m_tMajor.transform.forward = Stellar.StellarInst.GetDir(PathPoints, m_fCurPer);
+            m_tMajor.transform.forward = m_cStellar.GetDir(m_fCurPer);
             //增加进度.
             m_fCurPer += m_fSpeed * Time.deltaTime;
         }
-
-        void OnDrawGizmos()
-        {
-            if(PathPoints == null || PathPoints.Length == 0)
-                PathPoints = Stellar.StellarInst.InitializePathPoints(m_tMajorPathPoints);          //初始化路线
-
-            Stellar.StellarInst.GizmoDraw(PathPoints, m_fCurPer);
-        }
-
     }
 
 }
+
+
+//void OnDrawGizmos()
+//{
+//    if(PathPoints == null || PathPoints.Length == 0)
+//        PathPoints = Stellar.StellarInst.InitializePathPoints(m_tMajorPathPoints);          //初始化路线
+//    Stellar.StellarInst.GizmoDraw(PathPoints, m_fCurPer);
+//}
---
+++ Shader/TestBezierLine/Assets/Scripts/StellarCatmull/StellarInterface.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StellarInterface {
+
+    public static Vector3 Interp(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        Vector3 a = p0;
+        Vector3 b = p1;
+        Vector3 c = p2;
+        Vector3 d = p3;
+
+        float u = t;
+
+        return 0.5f * (
+           (-a + 3f * b - 3f * c + d) * (u * u * u)
+           + (2f * a - 5f * b + 4f * c - d) * (u * u)
+           + (-a + c) * u
+           + 2f * b
+       );
+
+    }
+
+    public static Vector3 Velocity(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        Vector3 a = p0;
+        Vector3 b = p1;
+        Vector3 c = p2;
+        Vector3 d = p3;
+
+        float u = t;
+
+        return 1.5f * (-a + 3f * b - 3f * c + d) * (u * u)
+              + (2f * a - 5f * b + 4f * c - d) * u
+              + .5f * c - .5f * a;
+
+    }
+
+}
